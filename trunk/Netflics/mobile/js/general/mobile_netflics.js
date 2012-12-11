ad973d00fba618c994f36bc042375bb46873d608
juniorvcm92@gmail.com <juniorvcm92@gmail.com@94aaf509-1e84-968d-57f1-943d4e7940b4>
2012-12-11 03:31:10 +0000

    Netflics Project Junior Venegas
    Adding basic structure for the mobile an full-site page

---
+++ trunk/Netflics/mobile/js/general/mobile_netflics.js
@@ -0,0 +1,13 @@
+/*movie data, actor and image API*/
+
+var Netflics = {};
+
+Netflics.Movies = function (allMovies) {
+};
+
+Netflics.Service = function () {
+
+};
+
+$(document).ready(function() {
+});
